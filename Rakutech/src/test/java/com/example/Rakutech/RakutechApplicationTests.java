12a4420eb27f190550c8b5f4be79d1090d36ecfc
james-99k <jamesgasty@gmail.com>
2021-06-22 20:44:58 +0800

    spring demo

---
+++ Rakutech/src/test/java/com/example/Rakutech/RakutechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Rakutech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RakutechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
