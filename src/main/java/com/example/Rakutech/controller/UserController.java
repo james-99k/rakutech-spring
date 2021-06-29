9b936dc409e72bbc4868d73e39d28f659f16553d
james-99k <jamesgasty@gmail.com>
2021-06-29 21:08:04 +0800

    register

--- src/main/java/com/example/Rakutech/controller/UserController.java
+++ src/main/java/com/example/Rakutech/controller/UserController.java
@@ -1,19 +1,35 @@
 package com.example.rakutech.controller;
 
+import com.example.rakutech.models.User;
 import com.example.rakutech.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
 
     @Autowired
-    private UserRepository userRepo;
+    private UserRepository repo;
 
     @GetMapping("")
     public String viewHomePage() {
         return "index";
+    }
+
+    @GetMapping("/register")
+    private String showSignUpForm(Model model) {
+        model.addAttribute("user", new User());
+
+        return "signup_form";
+    }
+
+    @PostMapping("/process_register")
+    public String processingRegistration(User user) {
+        repo.save(user);
+        return "register_success";
     }
 
 }
